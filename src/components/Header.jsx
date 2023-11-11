9e48d4b649b2f551f92e6a2587d506faef874d64
Ryan Mitchell <ryanm4398@gmail.com>
2023-11-10 22:48:22 -0500

    added image underneath Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,25 @@
 import '../styles/Header.css';
+import HoopersHeader from '../assets/images/hoop-header.jpg';
 
 const Header = () => {
     return (
         <>
             <header className='header'>
-                <div>
-                    <h1>Hooper</h1>
+                <div className='titleAndNav'>
+                    <div>
+                        <h1>Hooper</h1>
+                    </div>
+                    <nav className='nav'>
+                        <ul className='navItems'>
+                            <li><a href='/' className='navLink'>Home</a></li>
+                            <li><a href='/cart' className='navLink'>Cart</a></li>
+                            <li><a href='/stats' className='navLink'>Stats</a></li>
+                        </ul>
+                    </nav>
                 </div>
-                <nav className='nav'>
-                    <ul className='navItems'>
-                        <li><a href='/' className='navLink'>Home</a></li>
-                        <li><a href='/cart' className='navLink'>Cart</a></li>
-                        <li><a href='/stats' className='navLink'>Stats</a></li>
-                    </ul>
-                </nav>
-                
+                <div className='headerImage'>
+                    <img src={HoopersHeader} alt='Hoopers Header' width={'100%'} height={'350px'} />
+                </div>
             </header>
         </>
     );
